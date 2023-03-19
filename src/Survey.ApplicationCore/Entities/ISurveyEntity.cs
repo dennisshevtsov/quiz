4b3f802b343799b7962867cbb6c930ee8c901cb4
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-19 17:32:51 +0300

    add survey entity

---
+++ src/Survey.ApplicationCore/Entities/ISurveyEntity.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Survey.ApplicationCore.Entities
+{
+  public interface ISurveyEntity
+  {
+    public Guid SurveyId { get; }
+
+    public string Name { get; }
+
+    public string Description { get; }
+  }
+}
