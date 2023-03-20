8f115be3f2f7638bf1be59d364eb14d3bd59d776
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-20 13:08:24 +0300

    add survey repository

---
+++ src/Survey.ApplicationCore/Repositories/ISurveyRepository.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Survey.ApplicationCore.Repositories
+{
+  using Survey.ApplicationCore.Entities;
+
+  /// <summary>Provides a simple API to the storage of the survey entity.</summary>
+  public interface ISurveyRepository
+  {
+    /// <summary>Saves a new survey entity to the storage.</summary>
+    /// <param name="surveyEntity">An object that represents a survey entity.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that represents an asynchronous operation.</returns>
+    public Task InsertAsync(ISurveyEntity surveyEntity, CancellationToken cancellationToken);
+  }
+}
