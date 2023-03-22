bcd06db2bd48cf0e83f13f32e7b059f59cd6b72d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-22 23:38:13 +0300

    add unit test for survey controller

--- test/Survey.Test/Controllers/SurveyControllerTest.cs
+++ test/Survey.Test/Controllers/SurveyControllerTest.cs
@@ -4,7 +4,11 @@
 
 namespace Survey.Web.Controllers.Test
 {
+  using Microsoft.AspNetCore.Mvc;
+
+  using Survey.Domain.Entities;
   using Survey.Domain.Services;
+  using Survey.Web.ViewModels;
 
   [TestClass]
   public sealed class SurveyControllerTest
@@ -21,6 +25,33 @@ namespace Survey.Web.Controllers.Test
       _surveyServiceMock = new Mock<ISurveyService>();
 
       _surveyController = new SurveyController(_surveyServiceMock.Object);
+    }
+
+    [TestMethod]
+    public async Task AddSurvey_Should_Save_Survey()
+    {
+      _surveyServiceMock.Setup(service => service.AddNewSurveyAsync(It.IsAny<ISurveyData>(), It.IsAny<CancellationToken>()))
+                        .ReturnsAsync(new SurveyIdentity())
+                        .Verifiable();
+
+      var vm = new SurveyViewModel
+      {
+        Name = Guid.NewGuid().ToString(),
+        Description = Guid.NewGuid().ToString(),
+      };
+
+      var actionResult = await _surveyController.AddSurvey(vm, CancellationToken.None);
+
+      Assert.IsNotNull(actionResult);
+      Assert.IsInstanceOfType(actionResult, typeof(OkResult));
+
+      _surveyServiceMock.Verify(service => service.AddNewSurveyAsync(It.Is<ISurveyData>(data => data.Name == vm.Name && data.Description == vm.Description), CancellationToken.None));
+      _surveyServiceMock.VerifyNoOtherCalls();
+    }
+
+    private sealed class SurveyIdentity : ISurveyIdentity
+    {
+      public Guid SurveyId { get; set; }
     }
   }
 }
