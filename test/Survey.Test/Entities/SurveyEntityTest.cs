5d22cc2e776de1386b2b7d884f10ed79e67d5cd9
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-22 23:19:04 +0300

    add unit test for survey entity

---
+++ test/Survey.Test/Entities/SurveyEntityTest.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Survey.Application.Entities.Test
+{
+  using Survey.Web.ViewModels;
+
+  [TestClass]
+  public sealed class SurveyEntityTest
+  {
+    [TestMethod]
+    public void Constructor_Should_Copy_Data()
+    {
+      var surveyData = new SurveyViewModel
+      {
+        Name = Guid.NewGuid().ToString(),
+        Description = Guid.NewGuid().ToString(),
+      };
+
+      var surveyEntity = new SurveyEntity(surveyData);
+
+      Assert.AreEqual(surveyData.Name, surveyEntity.Name);
+      Assert.AreEqual(surveyData.Description, surveyEntity.Description);
+    }
+  }
+}
