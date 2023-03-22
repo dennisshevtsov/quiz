32bea02b78477b8dda6a70601a69f28f090b0f28
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-22 23:28:22 +0300

    add unit test for survey controller

---
+++ test/Survey.Test/Controllers/SurveyControllerTest.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Survey.Web.Controllers.Test
+{
+  using Survey.Domain.Services;
+
+  [TestClass]
+  public sealed class SurveyControllerTest
+  {
+#pragma warning disable CS8618
+    private Mock<ISurveyService> _surveyServiceMock;
+
+    private SurveyController _surveyController;
+#pragma warning restore CS8618
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      _surveyServiceMock = new Mock<ISurveyService>();
+
+      _surveyController = new SurveyController(_surveyServiceMock.Object);
+    }
+  }
+}
