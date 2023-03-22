5e4c8a18af692f88c51b723385af4bd7a4984d16
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-22 23:24:09 +0300

    add unit test for survey entity

--- test/Survey.Test/Entities/SurveyEntityTest.cs
+++ test/Survey.Test/Entities/SurveyEntityTest.cs
@@ -4,7 +4,7 @@
 
 namespace Survey.Application.Entities.Test
 {
-  using Survey.Web.ViewModels;
+  using Survey.Domain.Entities;
 
   [TestClass]
   public sealed class SurveyEntityTest
@@ -12,16 +12,37 @@ namespace Survey.Application.Entities.Test
     [TestMethod]
     public void Constructor_Should_Copy_Data()
     {
-      var surveyData = new SurveyViewModel
-      {
-        Name = Guid.NewGuid().ToString(),
-        Description = Guid.NewGuid().ToString(),
-      };
+      var testSurveyData = new TestSurveyData();
+      var testSurveyEntity = new SurveyEntity(testSurveyData);
 
-      var surveyEntity = new SurveyEntity(surveyData);
+      Assert.AreEqual(testSurveyData.Name, testSurveyEntity.Name);
+      Assert.AreEqual(testSurveyData.Description, testSurveyEntity.Description);
+    }
 
-      Assert.AreEqual(surveyData.Name, surveyEntity.Name);
-      Assert.AreEqual(surveyData.Description, surveyEntity.Description);
+    [TestMethod]
+    public void Constructor_Should_Copy_Entity()
+    {
+      var testSurveyEntity = new TestSurveyEntity();
+      var actualSurveyEntity = new SurveyEntity(testSurveyEntity);
+
+      Assert.AreEqual(testSurveyEntity.Name, actualSurveyEntity.Name);
+      Assert.AreEqual(testSurveyEntity.Description, actualSurveyEntity.Description);
+    }
+
+    private sealed class TestSurveyData : ISurveyData
+    {
+      public string Name { get; } = Guid.NewGuid().ToString();
+
+      public string Description { get; } = Guid.NewGuid().ToString();
+    }
+
+    private sealed class TestSurveyEntity : ISurveyEntity
+    {
+      public Guid SurveyId { get; } = Guid.NewGuid();
+
+      public string Name { get; } = Guid.NewGuid().ToString();
+
+      public string Description { get; } = Guid.NewGuid().ToString();
     }
   }
 }
