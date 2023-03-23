9928146270e33ec46653f89f7368ba7e5b0f3827
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-23 12:47:32 +0300

    add method to get survey to survey service

--- src/Survey.Application/Services/SurveyService.cs
+++ src/Survey.Application/Services/SurveyService.cs
@@ -35,5 +35,12 @@ namespace Survey.Application.Services
 
       return surveyEntity;
     }
+
+    /// <summary>Gets a survey.</summary>
+    /// <param name="surveyId">An object that represents an identity of a survey.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that represents an asynchronous operation that produces a result at some time in the future. The result is an instance of the <see cref="Survey.Domain.Entities.ISurveyEntity"/> class.</returns>
+    public async Task<ISurveyEntity?> GetSurveyAsync(Guid surveyId, CancellationToken cancellationToken)
+      => await _surveyRepository.GetSurveyAsync(surveyId, cancellationToken)
   }
 }
