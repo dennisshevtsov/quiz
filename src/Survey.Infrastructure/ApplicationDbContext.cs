959749e48a2322e7cd93522bccb19fbe293cc4b9
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-24 18:53:18 +0300

    register survey entity type configuration

--- src/Survey.Infrastructure/ApplicationDbContext.cs
+++ src/Survey.Infrastructure/ApplicationDbContext.cs
@@ -6,8 +6,16 @@ namespace Survey.Infrastructure
 {
   using Microsoft.EntityFrameworkCore;
 
+  using Survey.Infrastructure.Configurations;
+
   /// <summary>Represents a session with the database and can be used to query and save instances of your entities.</summary>
   public sealed class ApplicationDbContext : DbContext
   {
+    /// <summary>Configures the model.</summary>
+    /// <param name="modelBuilder">An object that provides a simple API surface for configuring the model.</param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.ApplyConfiguration(new SurveyEntityTypeConfiguration());
+    }
   }
 }
