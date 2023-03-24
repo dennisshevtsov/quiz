86f5b219877622eb3c867640de07ef05711179d7
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-24 18:41:58 +0300

    add db context

---
+++ src/Survey.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Survey.Infrastructure
+{
+  using Microsoft.EntityFrameworkCore;
+
+  /// <summary>Represents a session with the database and can be used to query and save instances of your entities.</summary>
+  public sealed class ApplicationDbContext : DbContext
+  {
+  }
+}
