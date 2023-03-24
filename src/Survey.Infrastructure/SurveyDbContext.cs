54ace90fa530eab52a9521b60f7da75455e2f928
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-24 19:21:08 +0300

    add contstructor with options to db context

--- src/Survey.Infrastructure/SurveyDbContext.cs
+++ src/Survey.Infrastructure/SurveyDbContext.cs
@@ -11,6 +11,12 @@ namespace Survey.Infrastructure
   /// <summary>Represents a session with the database and can be used to query and save instances of your entities.</summary>
   public sealed class SurveyDbContext : DbContext
   {
+    /// <summary>Initializes a new instance of the <see cref="Survey.Infrastructure.SurveyDbContext"/> class.</summary>
+    /// <param name="options">An object that retprents options to configure the DB context.</param>
+    public SurveyDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
     /// <summary>Configures the model.</summary>
     /// <param name="modelBuilder">An object that provides a simple API surface for configuring the model.</param>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
