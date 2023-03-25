ed4bfed60d386f48d30f3029ad5a3375246f4e4d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-25 19:16:02 +0300

    return created result in add survey action

--- src/Survey.Web/Controllers/SurveyController.cs
+++ src/Survey.Web/Controllers/SurveyController.cs
@@ -31,23 +31,23 @@ namespace Survey.Web.Controllers
     }
 
     /// <summary>Handles the add survey command request.</summary>
-    /// <param name="vm"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
+    /// <param name="vm">An object that represents a survey view model.</param>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that represents an asynchronous operation that produces a result at some time in the future. The result is an instance of the <see cref="Microsoft.AspNetCore.Mvc.IActionResult"/>.</returns>
     [HttpPost(Name = nameof(SurveyController.AddSurvey))]
-    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
     [Consumes(typeof(SurveyViewModel), ContentType.Json)]
     public async Task<IActionResult> AddSurvey([FromBody] SurveyViewModel vm, CancellationToken cancellationToken)
     {
-      await _surveyService.AddNewSurveyAsync(vm, cancellationToken);
+      var surveyIdentity = await _surveyService.AddNewSurveyAsync(vm, cancellationToken);
 
-      return Ok();
+      return CreatedAtAction(nameof(SurveyController.GetSurvey), new { surveyId = surveyIdentity.SurveyId }, null);
     }
 
     /// <summary>Handles the add survey command request.</summary>
     /// <param name="surveyId">An object that represents an identity of a survey.</param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
+    /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
+    /// <returns>An object that represents an asynchronous operation that produces a result at some time in the future. The result is an instance of the <see cref="Microsoft.AspNetCore.Mvc.IActionResult"/>.</returns>
     [HttpGet(SurveyController.GetSurveySubRoute, Name = nameof(SurveyController.GetSurvey))]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(SurveyViewModel), StatusCodes.Status200OK)]
