5c6cb90988e92297fff2848f65bb61e38a2c11d3
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-27 13:06:40 +0300

    add survey component to add survey component

--- src/survey-app/src/app/components/add-survey/add-survey.component.ts
+++ src/survey-app/src/app/components/add-survey/add-survey.component.ts
@@ -1,6 +1,20 @@
 import { Component } from '@angular/core';
 
+import { SurveyData         } from '../../entities';
+import { AddSurveyViewModel } from './add-survey.view-model';
+
 @Component({
   templateUrl: './add-survey.component.html',
+  providers: [AddSurveyViewModel],
 })
-export class AddSurveyComponent {}
+export class AddSurveyComponent {
+  public constructor(private readonly vm: AddSurveyViewModel) {}
+
+  public get survey(): SurveyData {
+    return this.vm.survey;
+  }
+
+  public ok(): void {
+    this.vm.add();
+  }
+}
