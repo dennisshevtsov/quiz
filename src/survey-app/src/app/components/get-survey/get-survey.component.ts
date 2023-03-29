8f9aad1c2bc62788eec42d1f09159d8347d59202
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-29 22:22:31 +0300

    add survey component to get surve component

--- src/survey-app/src/app/components/get-survey/get-survey.component.ts
+++ src/survey-app/src/app/components/get-survey/get-survey.component.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
 
+import { SurveyData } from 'src/app/entities';
+
 @Component({
   templateUrl: './get-survey.component.html',
 })
-export class GetSurveyComponent {}
+export class GetSurveyComponent {
+  public get survey(): SurveyData {
+    return {
+      name       : 'test',
+      description: 'test',
+    };
+  }
+
+  public ok(): void {}
+}
