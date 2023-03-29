facc4b1f4bf0a4cbb18d645fe0d59b982ffa1aa0
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-29 23:08:21 +0300

    fix tests

--- src/survey-app/src/app/components/update-survey/update-survey.component.spec.ts
+++ src/survey-app/src/app/components/update-survey/update-survey.component.spec.ts
@@ -1,13 +1,49 @@
-import { TestBed } from '@angular/core/testing';
+import { Component    } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Input        } from '@angular/core';
+import { Output       } from '@angular/core';
+import { TestBed      } from '@angular/core/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { SurveyData            } from '../../entities';
 import { UpdateSurveyComponent } from './update-survey.component';
+import { UpdateSurveyViewModel } from './update-survey.view-model';
 
-describe('GetSurveyComponent', () => {
+@Component({
+  selector: 'app-survey',
+})
+class TestSurveyComponent {
+  private okValue: EventEmitter<void>;
+
+  public constructor() {
+    this.okValue = new EventEmitter<void>();
+  }
+
+  @Input()
+  public set survey(value: SurveyData) {}
+
+  @Output()
+  public get ok(): EventEmitter<void> {
+    return this.okValue;
+  }
+}
+
+describe('UpdateSurveyComponent', () => {
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-                   declarations: [UpdateSurveyComponent]
-                 })
-                 .compileComponents();
+    TestBed.configureTestingModule({
+      declarations: [
+        UpdateSurveyComponent,
+        TestSurveyComponent,
+      ],
+      imports: [RouterTestingModule],
+    })
+
+    TestBed.overrideProvider(
+      UpdateSurveyViewModel,
+      {useValue: jasmine.createSpyObj(UpdateSurveyViewModel,['getSurvey'])});
+
+    await TestBed.compileComponents();
   });
 
   it('should create component', () => {
