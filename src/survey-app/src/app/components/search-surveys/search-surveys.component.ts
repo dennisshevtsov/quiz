f45763adbb725c0423aee4454f899ce4194465af
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-30 23:20:37 +0300

    add search surveys view model to search surveys components

--- src/survey-app/src/app/components/search-surveys/search-surveys.component.ts
+++ src/survey-app/src/app/components/search-surveys/search-surveys.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 
-import { SurveyEntity } from '../../entities';
+import { SurveyEntity           } from '../../entities';
+import { SearchSurveysViewModel } from './search-surveys.view-model';
 
 @Component({
   templateUrl: './search-surveys.component.html',
+  providers: [SearchSurveysViewModel],
 })
 export class SearchSurveysComponent {
+  public constructor(private readonly vm: SearchSurveysViewModel) {}
+
   public get surveys() : SurveyEntity[] {
-    return [];
+    return this.vm.surveys;
   }
 }
