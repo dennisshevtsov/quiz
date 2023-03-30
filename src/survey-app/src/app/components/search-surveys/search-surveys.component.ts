fe66ea25d5a36109fe865585879cac048a89b691
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-30 23:17:46 +0300

    add suvey table

--- src/survey-app/src/app/components/search-surveys/search-surveys.component.ts
+++ src/survey-app/src/app/components/search-surveys/search-surveys.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 
+import { SurveyEntity } from '../../entities';
+
 @Component({
   templateUrl: './search-surveys.component.html',
 })
-export class SearchSurveysComponent {}
+export class SearchSurveysComponent {
+  public get surveys() : SurveyEntity[] {
+    return [];
+  }
+}
