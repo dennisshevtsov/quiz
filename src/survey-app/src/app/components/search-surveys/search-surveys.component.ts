6325311bbe5f2b945e92e7ee3eb7fe55e99216ca
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-30 23:27:57 +0300

    load data in search survey component

--- src/survey-app/src/app/components/search-surveys/search-surveys.component.ts
+++ src/survey-app/src/app/components/search-surveys/search-surveys.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
+import { OnDestroy } from '@angular/core';
+import { OnInit    } from '@angular/core';
+
+import { Subscription } from 'rxjs';
 
 import { SurveyEntity           } from '../../entities';
 import { SearchSurveysViewModel } from './search-surveys.view-model';
 
 @Component({
   templateUrl: './search-surveys.component.html',
-  providers: [SearchSurveysViewModel],
+  providers: [
+    SearchSurveysViewModel,
+    { provide: Subscription, useFactory: () => new Subscription() },
+  ],
 })
-export class SearchSurveysComponent {
-  public constructor(private readonly vm: SearchSurveysViewModel) {}
+export class SearchSurveysComponent implements OnInit, OnDestroy {
+  public constructor(
+    private readonly vm : SearchSurveysViewModel,
+    private readonly sub: Subscription,
+  ) {}
+
+  public ngOnInit(): void {
+    this.sub.add(this.vm.initialize().subscribe());
+  }
+
+  public ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   public get surveys() : SurveyEntity[] {
     return this.vm.surveys;
