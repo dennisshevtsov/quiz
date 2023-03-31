373748b87322ec881cfa179618f42022c7601498
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-03-31 20:43:23 +0300

    remove copy constructor in add survey view model

--- src/Survey.Web/ViewModels/AddSurveyViewModel.cs
+++ src/Survey.Web/ViewModels/AddSurveyViewModel.cs
@@ -14,14 +14,6 @@ namespace Survey.Web.ViewModels
     /// <summary>Initializes a new instance of the <see cref="Survey.Web.ViewModels.AddSurveyViewModel"/> class.</summary>
     public AddSurveyViewModel() {}
 
-    /// <summary>Initializes a new instance of the <see cref="Survey.Web.ViewModels.AddSurveyViewModel"/> class.</summary>
-    /// <param name="surveyData">An object that represents survey data.</param>
-    public AddSurveyViewModel(ISurveyData surveyData)
-    {
-      Name = surveyData.Name;
-      Description = surveyData.Description;
-    }
-
     /// <summary>Gets an object that represents a name of a survey.</summary>
     [Required]
     public string Name { get; set; } = string.Empty;
