a45ba9cfdffba7894331b3f21fd7ed8c08fb9b63
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-01 20:47:15 +0300

    add angular material modules

--- src/survey-app/src/app/app.module.ts
+++ src/survey-app/src/app/app.module.ts
@@ -1,7 +1,17 @@
 import { HttpClientModule    } from '@angular/common/http';
 import { NgModule            } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms'
-import { BrowserModule       } from '@angular/platform-browser';
+
+import { BrowserModule           } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule    } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule      } from '@angular/material/icon';
+import { MatInputModule     } from '@angular/material/input';
+import { MatListModule      } from '@angular/material/list';
+import { MatSidenavModule   } from '@angular/material/sidenav';
+import { MatToolbarModule   } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent     } from './app.component';
@@ -21,8 +31,18 @@ import { UpdateSurveyComponent  } from './components';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     ReactiveFormsModule,
+
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+
     AppRoutingModule,
   ],
   providers: [],
