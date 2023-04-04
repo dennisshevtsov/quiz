f083ebfaa522f21edea702899bb1c33af337e76f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-04 23:19:22 +0300

    add integration test for add survey

--- test/Survey.Test/Integration/SurveyRepositoryTest.cs
+++ test/Survey.Test/Integration/SurveyRepositoryTest.cs
@@ -4,15 +4,18 @@
 
 namespace Survey.Infrastructure.Repositories.Test
 {
+  using System;
+
+  using Microsoft.EntityFrameworkCore;
   using Microsoft.Extensions.DependencyInjection;
 
   using Survey.Domain.Repositories;
+  using Survey.Infrastructure.Entities;
   using Survey.Test.Integration;
 
   [TestClass]
   public sealed class SurveyRepositoryTest : IntegrationTestBase
   {
-
 #pragma warning disable CS8618
     private ISurveyRepository _surveyRepository;
 #pragma warning restore CS8618
@@ -20,6 +23,46 @@ namespace Survey.Infrastructure.Repositories.Test
     protected override void InitializeInternal()
     {
       _surveyRepository = ServiceProvider.GetRequiredService<ISurveyRepository>();
+    }
+
+    [TestMethod]
+    public async Task AddSurveyAsync_Should_Create_New_Survey()
+    {
+      var controlSurveyEntity = SurveyRepositoryTest.GenerateTestSurvey();
+
+      await _surveyRepository.AddSurveyAsync(controlSurveyEntity, CancellationToken);
+
+      var actualSurveyEntity =
+        await DbContext.Set<SurveyEntity>()
+                       .AsNoTracking()
+                       .Where(entity => entity.SurveyId == controlSurveyEntity.SurveyId)
+                       .SingleOrDefaultAsync(CancellationToken);
+
+      Assert.IsNotNull(actualSurveyEntity);
+
+      SurveyRepositoryTest.AreEqual(controlSurveyEntity, actualSurveyEntity);
+      IsDetached(controlSurveyEntity);
+    }
+
+    private static SurveyEntity GenerateTestSurvey() => new()
+    {
+      SurveyId    = Guid.NewGuid(),
+      Name        = Guid.NewGuid().ToString(),
+      Description = Guid.NewGuid().ToString(),
+    };
+
+    private static void AreEqual(SurveyEntity control, SurveyEntity actual)
+    {
+      Assert.AreEqual(control.SurveyId, actual.SurveyId);
+      Assert.AreEqual(control.Name, actual.Name);
+      Assert.AreEqual(control.Description, actual.Description);
+    }
+
+    private void IsDetached(SurveyEntity entity)
+    {
+      var entry = DbContext.Entry(entity);
+
+      Assert.AreEqual(EntityState.Detached, entry.State);
     }
   }
 }
