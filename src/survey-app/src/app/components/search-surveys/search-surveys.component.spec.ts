6adf11ad3b516bd094d2e659bdac0a79498ffd2a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-05 23:29:23 +0300

    fix test for search survey component

--- src/survey-app/src/app/components/search-surveys/search-surveys.component.spec.ts
+++ src/survey-app/src/app/components/search-surveys/search-surveys.component.spec.ts
@@ -1,10 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed                 } from '@angular/core/testing';
+import { RouterTestingModule     } from '@angular/router/testing';
+
+import { MatIconModule  } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 
 import { SearchSurveysComponent } from './search-surveys.component';
 
 describe('SearchSurveysComponent', () => {
   beforeEach(async () => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        MatIconModule,
+        MatTableModule,
+        RouterTestingModule,
+      ],
       declarations: [SearchSurveysComponent],
     });
 
