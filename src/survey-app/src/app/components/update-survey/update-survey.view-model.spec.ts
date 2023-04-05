7255e6ccec577658a2ded63c9c58e15e5166d907
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-06 00:18:14 +0300

    add unit test for update survey view model

--- src/survey-app/src/app/components/update-survey/update-survey.view-model.spec.ts
+++ src/survey-app/src/app/components/update-survey/update-survey.view-model.spec.ts
@@ -12,7 +12,7 @@ describe('UpdateSurveyViewModel', () => {
       providers: [
         UpdateSurveyViewModel,
         {
-          provide: SurveyService,
+          provide : SurveyService,
           useValue: jasmine.createSpyObj(SurveyService, ['getSurvey', 'updateSurvey']),
         },
       ],
@@ -46,6 +46,32 @@ describe('UpdateSurveyViewModel', () => {
       expect(vm.survey)
         .withContext('survey should be populated')
         .toEqual(controlSurvey);
+    });
+  }));
+
+  it('should update survey', inject([UpdateSurveyViewModel, SurveyService], (vm: UpdateSurveyViewModel, srv: jasmine.SpyObj<SurveyService>) => {
+    const surveyId          = 'test id';
+    const surveyName        = 'test name';
+    const surveyDescription = 'test description';
+
+    srv.updateSurvey.and.returnValue(of(void 0));
+
+    vm.survey.surveyId    = surveyId;
+    vm.survey.name        = surveyName;
+    vm.survey.description = surveyDescription;
+
+    vm.update().subscribe(() => {
+      expect(srv.updateSurvey.calls.count())
+        .withContext('updateSurvey should be called once')
+        .toBe(1);
+
+      expect(srv.updateSurvey.calls.first().args[0])
+        .withContext('updateSurvey should receive surveyId')
+        .toEqual({
+          surveyId   : surveyId,
+          name       : surveyName,
+          description: surveyDescription,
+        });
     });
   }));
 });
