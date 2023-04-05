6cfb2275508c1d40d01593c8f4f9ae7c297ee2ee
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-06 00:26:29 +0300

    add unit test for search surveys view model

--- src/survey-app/src/app/components/search-surveys/search-surveys.view-model.spec.ts
+++ src/survey-app/src/app/components/search-surveys/search-surveys.view-model.spec.ts
@@ -18,4 +18,30 @@ describe('SearchSurveysViewModel', () => {
       ],
     });
   });
+
+  it('should load surveys', inject([SearchSurveysViewModel, SurveyService], (vm: SearchSurveysViewModel, srv: jasmine.SpyObj<SurveyService>) => {
+    const surveyId          = 'test id';
+    const surveyName        = 'test name';
+    const surveyDescription = 'test description';
+
+    const controlSurveyCollection = [{
+      surveyId   : surveyId,
+      name       : surveyName,
+      description: surveyDescription,
+    }];
+
+    srv.searchSurveys.and.returnValue(of({
+      surveys: controlSurveyCollection,
+    }));
+
+    vm.initialize().subscribe(() => {
+      expect(srv.searchSurveys.calls.count())
+        .withContext('searchSurveys should be called once')
+        .toBe(1);
+
+      expect(vm.surveys)
+        .withContext('surveys should be populated')
+        .toEqual(controlSurveyCollection);
+    });
+  }));
 });
