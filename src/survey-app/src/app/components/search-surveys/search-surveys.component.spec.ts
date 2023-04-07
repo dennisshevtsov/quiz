2866bbb1081ce24a32804f397d6a8df7c195834f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-07 20:52:24 +0300

    add test for init in searhc surveys component

--- src/survey-app/src/app/components/search-surveys/search-surveys.component.spec.ts
+++ src/survey-app/src/app/components/search-surveys/search-surveys.component.spec.ts
@@ -1,11 +1,14 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TestBed                 } from '@angular/core/testing';
+import { TestBed, fakeAsync, inject, tick                 } from '@angular/core/testing';
 import { RouterTestingModule     } from '@angular/router/testing';
 
 import { MatIconModule  } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 
+import { Subscription, of } from 'rxjs';
+
 import { SearchSurveysComponent } from './search-surveys.component';
+import { SearchSurveysViewModel } from './search-surveys.view-model';
 
 describe('SearchSurveysComponent', () => {
   beforeEach(async () => {
@@ -19,6 +22,16 @@ describe('SearchSurveysComponent', () => {
       declarations: [SearchSurveysComponent],
     });
 
+    TestBed.overrideProvider(
+      Subscription,
+      { useValue: jasmine.createSpyObj(Subscription, ['add', 'unsubscribe'])},
+    );
+
+    const vm = jasmine.createSpyObj('SearchSurveysViewModel', ['initialize'], ['surveys']);
+    vm.initialize.and.returnValue(of(void 0));
+
+    TestBed.overrideProvider(SearchSurveysViewModel, { useValue: vm });
+
     await TestBed.compileComponents();
   });
 
@@ -30,4 +43,20 @@ describe('SearchSurveysComponent', () => {
 
     expect(component).toBeTruthy();
   });
+
+  it('ngOnInit should initialize component', fakeAsync(inject([Subscription, SearchSurveysViewModel], (sub: jasmine.SpyObj<Subscription>, vm: jasmine.SpyObj<SearchSurveysViewModel>) => {
+    const fixture = TestBed.createComponent(SearchSurveysComponent);
+
+    fixture.detectChanges();
+
+    tick();
+
+    expect(sub.add.calls.count())
+      .withContext('sub.add should be called')
+      .toBe(1);
+
+    expect(vm.initialize.calls.count())
+      .withContext('vm.initialize should be called')
+      .toBe(1);
+  })));
 });
