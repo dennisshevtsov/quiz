52ee4a34ac85f3cbc25523b1ea8a5abc09430687
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-08 19:01:16 +0300

    add test for ngondestroy in add survey component

--- src/survey-app/src/app/components/add-survey/add-survey.component.spec.ts
+++ src/survey-app/src/app/components/add-survey/add-survey.component.spec.ts
@@ -2,7 +2,16 @@ import { Component    } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 import { Input        } from '@angular/core';
 import { Output       } from '@angular/core';
-import { TestBed      } from '@angular/core/testing';
+
+import { fakeAsync } from '@angular/core/testing';
+import { inject    } from '@angular/core/testing';
+import { TestBed   } from '@angular/core/testing';
+import { tick      } from '@angular/core/testing';
+
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { Subscription } from 'rxjs';
+import { of           } from 'rxjs';
 
 import { SurveyData         } from '../../entities';
 import { AddSurveyComponent } from './add-survey.component';
@@ -34,11 +43,18 @@ describe('AddSurveyComponent', () => {
         TestSurveyComponent,
         AddSurveyComponent,
       ],
+      imports: [RouterTestingModule],
     });
 
+    const vm = jasmine.createSpyObj('AddSurveyViewModel', ['add'], ['survey']);
+    vm.add.and.returnValue(of(void 0));
+
+    TestBed.overrideProvider(AddSurveyViewModel, {useValue: vm});
+
     TestBed.overrideProvider(
-      AddSurveyViewModel,
-      {useValue: jasmine.createSpyObj(AddSurveyViewModel,['addSurvey'])});
+      Subscription,
+      { useValue: jasmine.createSpyObj(Subscription, ['add', 'unsubscribe'])},
+    );
 
     await TestBed.compileComponents();
   });
@@ -51,4 +67,21 @@ describe('AddSurveyComponent', () => {
 
     expect(component).toBeTruthy();
   });
+
+  it('should unsubsribe in ngOnDestroy', fakeAsync(inject(
+    [Subscription, AddSurveyViewModel],
+    (sub: jasmine.SpyObj<Subscription>,
+     vm: jasmine.SpyObj<AddSurveyViewModel>) => {
+    const fixture = TestBed.createComponent(AddSurveyComponent);
+
+    fixture.detectChanges();
+
+    tick();
+
+    fixture.componentInstance.ngOnDestroy();
+
+    expect(sub.unsubscribe.calls.count())
+      .withContext('sub.unsubscribe should be called')
+      .toBe(1);
+  })));
 });
