f84a8225ac483c48fb9d0947b22ce0ddd6c1fb1a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-09 17:15:23 +0300

    show messages in add survey component

--- src/survey-app/src/app/components/add-survey/add-survey.component.ts
+++ src/survey-app/src/app/components/add-survey/add-survey.component.ts
@@ -3,6 +3,8 @@ import { OnDestroy } from '@angular/core';
 
 import { Router } from '@angular/router';
 
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 import { Subscription } from 'rxjs';
 
 import { SurveyData         } from '../../entities';
@@ -20,6 +22,7 @@ export class AddSurveyComponent implements OnDestroy {
     private readonly vm    : AddSurveyViewModel,
     private readonly sub   : Subscription,
     private readonly router: Router,
+    private readonly sb    : MatSnackBar,
   ) {}
 
   public ngOnDestroy(): void {
@@ -32,8 +35,11 @@ export class AddSurveyComponent implements OnDestroy {
 
   public ok(): void {
     this.sub.add(this.vm.add().subscribe({
-      complete: () => this.router.navigate(['survey', this.vm.survey.surveyId]),
-      error   : () => {},
+      complete: () => {
+        this.sb.open('The new survey is created.', 'Undo');
+        this.router.navigate(['survey', this.vm.survey.surveyId]);
+      },
+      error   : () => this.sb.open('An error occured.', 'Undo'),
     }));
   }
 }
