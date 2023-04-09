cf1b8c2d4a155f326e3b44c439d744e5a80826c6
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-09 17:08:15 +0300

    add snack bar module

--- src/survey-app/src/app/app.module.ts
+++ src/survey-app/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatIconModule      } from '@angular/material/icon';
 import { MatInputModule     } from '@angular/material/input';
 import { MatListModule      } from '@angular/material/list';
 import { MatSidenavModule   } from '@angular/material/sidenav';
+import { MatSnackBarModule  } from '@angular/material/snack-bar';
 import { MatTableModule     } from '@angular/material/table';
 import { MatToolbarModule   } from '@angular/material/toolbar';
 
@@ -42,6 +43,7 @@ import { UpdateSurveyComponent  } from './components';
     MatInputModule,
     MatListModule,
     MatSidenavModule,
+    MatSnackBarModule,
     MatTableModule,
     MatToolbarModule,
 
