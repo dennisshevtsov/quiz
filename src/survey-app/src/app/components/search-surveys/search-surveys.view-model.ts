ad4e505423f25d8ced5469a73f71ea3e95f26e4f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-11 22:41:30 +0300

    add delete survey to view model

--- src/survey-app/src/app/components/search-surveys/search-surveys.view-model.ts
+++ src/survey-app/src/app/components/search-surveys/search-surveys.view-model.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@angular/core';
 
 import { map        } from 'rxjs';
 import { Observable } from 'rxjs';
+import { switchMap  } from 'rxjs';
 
-import { SurveyService } from '../../services';
-import { SurveyEntity  } from '../../entities';
+import { SurveyEntity   } from '../../entities';
+import { SurveyIdentity } from '../../entities';
+import { SurveyService  } from '../../services';
 
 @Injectable()
 export class SearchSurveysViewModel {
@@ -21,5 +23,10 @@ export class SearchSurveysViewModel {
                        .pipe(map(collection => {
                          this.surveysValue = collection.surveys;
                        }));
+  }
+
+  public delete(survey: SurveyIdentity): Observable<void> {
+    return this.service.deleteSurvey(survey)
+                       .pipe(switchMap(this.initialize));
   }
 }
