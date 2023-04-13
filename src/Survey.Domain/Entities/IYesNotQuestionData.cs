c33a24eac499bd5cfebd36801a440cb63eb3527b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-04-13 23:13:29 +0300

    add yes no question data

---
+++ src/Survey.Domain/Entities/IYesNotQuestionData.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Survey.Domain.Entities
+{
+  /// <summary>Represents yes/no question data.</summary>
+  public interface IYesNotQuestionData
+  {
+  }
+}
