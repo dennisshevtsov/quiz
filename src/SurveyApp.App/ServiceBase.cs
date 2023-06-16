38ffe3008094c9f89eccdffa3240d1fa3de6a474
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-06-16 16:46:10 +0300

    add service base

--- src/SurveyApp.App/ServiceBase.cs
+++ src/SurveyApp.App/ServiceBase.cs
@@ -2,38 +2,67 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
-namespace SurveyApp;
+namespace SurveyApp.App;
 
 /// <summary>Provides a simple GRUD API.</summary>
+/// <typeparam name="TBusinessEntity">A business entity.</typeparam>
 /// <typeparam name="TEntity">An entity type.</typeparam>
 /// <typeparam name="TIdentity">An identity type.</typeparam>
-public interface IService<TEntity, TIdentity>
+public abstract class ServiceBase<TBusinessEntity, TEntity, TIdentity> : IService<TEntity, TIdentity>
+  where TBusinessEntity : EntityBase, TEntity
+  where TEntity : TIdentity
 {
+  private readonly IRepository<TEntity, TIdentity> _repository;
+
+  /// <summary>Initializes a new instance of the <see cref="BookApi.App.ServiceBase{TEntity, TData, TEntity}"/> class.</summary>
+  /// <param name="repository">An object that provides a simple API to store instances of the <see cref="TEntity"/>.</param>
+  protected ServiceBase(IRepository<TEntity, TIdentity> repository)
+  {
+    _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+  }
+
   /// <summary>Gets an entity.</summary>
   /// <param name="identity">An object that represents an identity to get.</param>
   /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
   /// <returns>An object that represents an asynchronous operation that produces a result at some time in the future. The result is an instance of the <see cref="BookApi.Author.IAuthorEntity"/>. The result can be null.</returns>
-  public Task<TEntity?> GetAsync(TIdentity identity, CancellationToken cancellationToken);
+  public virtual Task<TEntity?> GetAsync(TIdentity identity, CancellationToken cancellationToken)
+  {
+    var entity = EntityBase.Create<TIdentity, TBusinessEntity>(identity);
+
+    return _repository.GetAsync(identity, entity.Relations(), cancellationToken);
+  }
 
   /// <summary>Gets an entity.</summary>
   /// <param name="identity">An object that represents an identity to get.</param>
   /// <param name="relations">An object that represents a collection of relations to load.</param>
   /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
   /// <returns>An object that represents an asynchronous operation that produces a result at some time in the future. The result is an instance of the <see cref="BookApi.Author.IAuthorEntity"/>. The result can be null.</returns>
-  public Task<TEntity?> GetAsync(TIdentity identity, IEnumerable<string> relations, CancellationToken cancellationToken);
+  public Task<TEntity?> GetAsync(TIdentity identity, IEnumerable<string> relations, CancellationToken cancellationToken)
+  {
+    var entity = EntityBase.Create<TIdentity, TBusinessEntity>(identity);
+
+    return _repository.GetAsync(identity, entity.Relations(relations), cancellationToken);
+  }
 
   /// <summary>Adds an entity.</summary>
-  /// <param name="data">An object that represents an entity from that a new entity should be created.</param>
+  /// <param name="entity">An object that represents data from that a new entity should be created.</param>
   /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
   /// <returns>An object that represents an asynchronous operation that produces a result at some time in the future. The result is an instance of the <see cref="BookApi.Author.IAuthorEntity"/>.</returns>
-  public Task<TEntity> AddAsync(TEntity data, CancellationToken cancellationToken);
+  public virtual Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken)
+    => _repository.AddAsync(entity, cancellationToken);
 
   /// <summary>Updates an entity.</summary>
   /// <param name="originalEntity">An object that represents an entity to update.</param>
   /// <param name="newEntity">An object that represents an entity from that the original one should be updated.</param>
   /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
   /// <returns>An object that represents an asynchronous operation.</returns>
-  public Task UpdateAsync(TEntity originalEntity, TEntity newEntity, CancellationToken cancellationToken);
+  public virtual Task UpdateAsync(TEntity originalEntity, TEntity newEntity, CancellationToken cancellationToken)
+  {
+    var businessEntity = EntityBase.Create<TEntity, TBusinessEntity>(originalEntity);
+    var updatedProperties = businessEntity.Update(newEntity!);
+
+    return _repository.UpdateAsync(originalEntity, newEntity, updatedProperties, cancellationToken);
+  }
 
   /// <summary>Updates an entity partially.</summary>
   /// <param name="originalEntity">An object that represents an entity to update.</param>
@@ -41,11 +70,18 @@ public interface IService<TEntity, TIdentity>
   /// <param name="properties">An object that represents a collection of properties to update.</param>
   /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
   /// <returns>An object that represents an asynchronous operation.</returns>
-  public Task UpdateAsync(TEntity originalEntity, TEntity newEntity, IEnumerable<string> properties, CancellationToken cancellationToken);
+  public virtual Task UpdateAsync(TEntity originalEntity, TEntity newEntity, IEnumerable<string> properties, CancellationToken cancellationToken)
+  {
+    var businessEntity = EntityBase.Create<TEntity, TBusinessEntity>(originalEntity);
+    var updatedProperties = businessEntity.Update(newEntity!, properties);
+
+    return _repository.UpdateAsync(originalEntity, newEntity, updatedProperties, cancellationToken);
+  }
 
   /// <summary>Deletes an entity.</summary>
   /// <param name="identity">An object that represents an identity to delete.</param>
   /// <param name="cancellationToken">An object that propagates notification that operations should be canceled.</param>
   /// <returns>An object that represents an asynchronous operation.</returns>
-  public Task DeleteAsync(TIdentity identity, CancellationToken cancellationToken);
+  public virtual Task DeleteAsync(TIdentity identity, CancellationToken cancellationToken)
+    => _repository.DeleteAsync(identity, cancellationToken);
 }
