f3e66f11f04283b0743b3f061b600781b8900905
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-06-18 00:03:51 +0300

    add survey controller

---
+++ src/SurveyApp.Web/Survey/SurveyController.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc;
+
+using SurveyApp.Web.Defaults;
+
+namespace SurveyApp.Survey.Web;
+
+/// <summary>Provides a simple API to handle HTTP request.</summary>
+[ApiController]
+[Route("api/survey")]
+[Produces(ContentType.Json)]
+public sealed class SurveyController : ControllerBase
+{
+}
