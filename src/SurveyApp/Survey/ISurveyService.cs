b3d90b951b4ef099d444cc3394f1dd5d29f30c3f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-06-17 12:40:07 +0300

    add survey service

---
+++ src/SurveyApp/Survey/ISurveyService.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.Survey;
+
+/// <summary>Provides a simple GRUD API for the <see cref="SurveyApp.Survey.ISurveyEntity"/>.</summary>
+public interface ISurveyService : IService<ISurveyEntity, ISurveyIdentity> { }
