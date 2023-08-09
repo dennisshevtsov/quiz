f65a1a0fe8a00dd31ececc18791326add89721d2
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-09 19:05:50 +0300

    clean solution

--- src/SurveyApp/Survey/ISurveyRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
-// Licensed under the MIT License.
-// See LICENSE in the project root for license information.
-
-namespace SurveyApp.Survey;
-
-/// <summary>Provides a simple API to persistence of the <see cref="SurveyApp.Survey.ISurveyEntity"/>.</summary>
-public interface ISurveyRepository : IRepository<ISurveyEntity, ISurveyIdentity> { }
--- src/SurveyApp/Survey/ISurveyService.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
-// Licensed under the MIT License.
-// See LICENSE in the project root for license information.
-
-namespace SurveyApp.Survey;
-
-/// <summary>Provides a simple GRUD API for the <see cref="SurveyApp.Survey.ISurveyEntity"/>.</summary>
-public interface ISurveyService : IService<ISurveyEntity, ISurveyIdentity> { }
