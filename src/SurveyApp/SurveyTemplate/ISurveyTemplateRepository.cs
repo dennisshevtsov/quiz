3dd686465c0da7442cc393ac1f7e165c2248a615
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-09 19:31:32 +0300

    add repository

---
+++ src/SurveyApp/SurveyTemplate/ISurveyTemplateRepository.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.SurveyTemplate;
+
+public interface ISurveyTemplateRepository
+{
+  public Task AddSurveyTemplateAsync(SurveyTemplateEntity surveyTemplateEntity, CancellationToken cancellationToken);
+
+  public Task GetSurveyTemplateAsync(Guid surveyTemplateId, CancellationToken cancellationToken);
+}
