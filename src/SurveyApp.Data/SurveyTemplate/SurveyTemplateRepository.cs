3cf669f249035f85351a0d7d236dfbdfc3b5dd10
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-10 23:11:36 +0300

    add survey template repository

---
+++ src/SurveyApp.Data/SurveyTemplate/SurveyTemplateRepository.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.SurveyTemplate.Data;
+
+public sealed class SurveyTemplateRepository : ISurveyTemplateRepository
+{
+  public Task AddSurveyTemplateAsync(SurveyTemplateEntity surveyTemplateEntity, CancellationToken cancellationToken) => Task.CompletedTask;
+
+  public Task GetSurveyTemplateAsync(Guid surveyTemplateId, CancellationToken cancellationToken) => Task.CompletedTask;
+}
