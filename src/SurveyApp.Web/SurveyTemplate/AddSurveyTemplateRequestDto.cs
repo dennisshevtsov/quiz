50c9acc7482e2ef99835cfa5f5e212d96c8f3189
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-12 19:47:43 +0300

    add add survey template request dto

---
+++ src/SurveyApp.Web/SurveyTemplate/AddSurveyTemplateRequestDto.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.Web.SurveyTemplate;
+
+public sealed class AddSurveyTemplateRequestDto
+{
+  public string Title { get; set; } = string.Empty;
+
+  public string Description { get; set; } = string.Empty;
+
+
+}
