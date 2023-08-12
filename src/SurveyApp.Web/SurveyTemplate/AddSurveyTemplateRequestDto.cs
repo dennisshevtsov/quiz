f37b9e18055701974ba2e5b3146b304be7cd1f8f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-12 19:51:08 +0300

    add survey template questions

--- src/SurveyApp.Web/SurveyTemplate/AddSurveyTemplateRequestDto.cs
+++ src/SurveyApp.Web/SurveyTemplate/AddSurveyTemplateRequestDto.cs
@@ -10,5 +10,5 @@ public sealed class AddSurveyTemplateRequestDto
 
   public string Description { get; set; } = string.Empty;
 
-
+  public SurveyTemplateQuestionDtoBase[] Questions { get; set; } = Array.Empty<SurveyTemplateQuestionDtoBase>();
 }
---
+++ src/SurveyApp.Web/SurveyTemplate/SurveyTemplateQuestionDtoBase.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using SurveyApp.SurveyTemplate;
+
+namespace SurveyApp.Web.SurveyTemplate;
+
+public abstract class SurveyTemplateQuestionDtoBase
+{
+  public string Text { get; set; } = string.Empty;
+
+  public abstract SurveyQuestionType QuestionType { get; }
+}
