a182dec5b12ba2e8b41566bbe4cf6677974507ed
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-13 12:22:08 +0300

    specify derived types

--- src/SurveyApp.Web/SurveyTemplate/SurveyTemplateQuestionDtoBase.cs
+++ src/SurveyApp.Web/SurveyTemplate/SurveyTemplateQuestionDtoBase.cs
@@ -2,8 +2,14 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using System.Text.Json.Serialization;
+
 namespace SurveyApp.SurveyTemplate.Web;
 
+[JsonDerivedType(typeof(MultipleChoiceQuestionTemplateDto))]
+[JsonDerivedType(typeof(SingleChoiceQuestionTemplateDto))]
+[JsonDerivedType(typeof(TextAreaQuestionTemplateDto))]
+[JsonDerivedType(typeof(YesNoQuestionTemplateDto))]
 public abstract class SurveyTemplateQuestionDtoBase
 {
   public string Text { get; set; } = string.Empty;
