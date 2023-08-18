dcedb9fafe22cdd15afac65097cdd25463adf5de
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-18 20:53:34 +0300

    add test for survey template controller

---
+++ test/SurveyApp.Test/Web/SurveyTemplate/SurveyTemplateControllerTest.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.SurveyTemplate.Web.Test;
+
+[TestClass]
+public sealed class SurveyTemplateControllerTest
+{
+#pragma warning disable CS8618
+  private Mock<ISurveyTemplateRepository> _surveyTemplateRepositoryMock;
+  private SurveyTemplateController _surveyTemplateController;
+#pragma warning restore CS8618
+
+  [TestInitialize]
+  public void Initialize()
+  {
+    _surveyTemplateRepositoryMock = new Mock<ISurveyTemplateRepository>();
+    _surveyTemplateController = new SurveyTemplateController(_surveyTemplateRepositoryMock.Object);
+  }
+}
