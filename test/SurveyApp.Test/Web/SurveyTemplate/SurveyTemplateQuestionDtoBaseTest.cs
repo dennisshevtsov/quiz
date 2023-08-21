8ecd53a626a34b3c91ed8d54912251a1b976b38c
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-21 22:25:58 +0300

    add tests for survey template question dto

--- test/SurveyApp.Test/Web/SurveyTemplate/SurveyTemplateQuestionDtoBaseTest.cs
+++ test/SurveyApp.Test/Web/SurveyTemplate/SurveyTemplateQuestionDtoBaseTest.cs
@@ -8,58 +8,114 @@ namespace SurveyApp.SurveyTemplate.Web.Test;
 public sealed class SurveyTemplateQuestionDtoBaseTest
 {
   [TestMethod]
-  public void FromQuestionTemplateEntity_TextQuestionTemplateEntity_SurveyTemplateQuestionDtoCreated()
+  public void FromQuestionTemplateEntity_TextQuestionTemplateEntity_SurveyTemplateQuestionDtoBaseCreated()
   {
     // Arrange
     TextQuestionTemplateEntity textQuestionTemplateEntity = new();
 
     // Act
-    SurveyTemplateQuestionDtoBase getSurveyTemplateResponseDto =
+    SurveyTemplateQuestionDtoBase surveyTemplateQuestionDtoBase =
       SurveyTemplateQuestionDtoBase.FromQuestionTemplateEntity(textQuestionTemplateEntity);
 
     // Assert
-    Assert.IsNotNull(getSurveyTemplateResponseDto);
+    Assert.IsNotNull(surveyTemplateQuestionDtoBase);
   }
 
   [TestMethod]
-  public void FromQuestionTemplateEntity_YesNoQuestionTemplateEntity_SurveyTemplateQuestionDtoCreated()
+  public void FromQuestionTemplateEntity_TextQuestionTemplateEntity_TextQuestionTemplateDtoCreated()
+  {
+    // Arrange
+    TextQuestionTemplateEntity textQuestionTemplateEntity = new();
+
+    // Act
+    SurveyTemplateQuestionDtoBase surveyTemplateQuestionDtoBase =
+      SurveyTemplateQuestionDtoBase.FromQuestionTemplateEntity(textQuestionTemplateEntity);
+
+    // Assert
+    Assert.IsInstanceOfType(surveyTemplateQuestionDtoBase, typeof(TextQuestionTemplateDto));
+  }
+
+  [TestMethod]
+  public void FromQuestionTemplateEntity_YesNoQuestionTemplateEntity_SurveyTemplateQuestionDtoBaseCreated()
   {
     // Arrange
     YesNoQuestionTemplateEntity yesNoQuestionTemplateEntity = new();
 
     // Act
-    SurveyTemplateQuestionDtoBase getSurveyTemplateResponseDto =
+    SurveyTemplateQuestionDtoBase surveyTemplateQuestionDtoBase =
       SurveyTemplateQuestionDtoBase.FromQuestionTemplateEntity(yesNoQuestionTemplateEntity);
 
     // Assert
-    Assert.IsNotNull(getSurveyTemplateResponseDto);
+    Assert.IsNotNull(surveyTemplateQuestionDtoBase);
   }
 
   [TestMethod]
-  public void FromQuestionTemplateEntity_MultipleChoiceQuestionTemplateEntity_SurveyTemplateQuestionDtoCreated()
+  public void FromQuestionTemplateEntity_YesNoQuestionTemplateEntity_YesNoQuestionTemplateDtoCreated()
+  {
+    // Arrange
+    YesNoQuestionTemplateEntity yesNoQuestionTemplateEntity = new();
+
+    // Act
+    SurveyTemplateQuestionDtoBase surveyTemplateQuestionDtoBase =
+      SurveyTemplateQuestionDtoBase.FromQuestionTemplateEntity(yesNoQuestionTemplateEntity);
+
+    // Assert
+    Assert.IsInstanceOfType(surveyTemplateQuestionDtoBase, typeof(YesNoQuestionTemplateDto));
+  }
+
+  [TestMethod]
+  public void FromQuestionTemplateEntity_MultipleChoiceQuestionTemplateEntity_SurveyTemplateQuestionDtoBaseCreated()
   {
     // Arrange
     MultipleChoiceQuestionTemplateEntity multipleChoiceQuestionTemplateEntity = new();
 
     // Act
-    SurveyTemplateQuestionDtoBase getSurveyTemplateResponseDto =
+    SurveyTemplateQuestionDtoBase surveyTemplateQuestionDtoBase =
       SurveyTemplateQuestionDtoBase.FromQuestionTemplateEntity(multipleChoiceQuestionTemplateEntity);
 
     // Assert
-    Assert.IsNotNull(getSurveyTemplateResponseDto);
+    Assert.IsNotNull(surveyTemplateQuestionDtoBase);
   }
 
   [TestMethod]
-  public void FromQuestionTemplateEntity_SingleChoiceQuestionTemplateEntity_SurveyTemplateQuestionDtoCreated()
+  public void FromQuestionTemplateEntity_MultipleChoiceQuestionTemplateEntity_MultipleChoiceQuestionTemplateDtoCreated()
+  {
+    // Arrange
+    MultipleChoiceQuestionTemplateEntity multipleChoiceQuestionTemplateEntity = new();
+
+    // Act
+    SurveyTemplateQuestionDtoBase surveyTemplateQuestionDtoBase =
+      SurveyTemplateQuestionDtoBase.FromQuestionTemplateEntity(multipleChoiceQuestionTemplateEntity);
+
+    // Assert
+    Assert.IsInstanceOfType(surveyTemplateQuestionDtoBase, typeof(MultipleChoiceQuestionTemplateDto));
+  }
+
+  [TestMethod]
+  public void FromQuestionTemplateEntity_SingleChoiceQuestionTemplateEntity_SurveyTemplateQuestionDtoBaseCreated()
   {
     // Arrange
     SingleChoiceQuestionTemplateEntity singleChoiceQuestionTemplateEntity = new();
 
     // Act
-    SurveyTemplateQuestionDtoBase getSurveyTemplateResponseDto =
+    SurveyTemplateQuestionDtoBase surveyTemplateQuestionDtoBase =
       SurveyTemplateQuestionDtoBase.FromQuestionTemplateEntity(singleChoiceQuestionTemplateEntity);
 
     // Assert
-    Assert.IsNotNull(getSurveyTemplateResponseDto);
+    Assert.IsNotNull(surveyTemplateQuestionDtoBase);
+  }
+
+  [TestMethod]
+  public void FromQuestionTemplateEntity_SingleChoiceQuestionTemplateEntity_SingleChoiceQuestionTemplateDtoCreated()
+  {
+    // Arrange
+    SingleChoiceQuestionTemplateEntity singleChoiceQuestionTemplateEntity = new();
+
+    // Act
+    SurveyTemplateQuestionDtoBase surveyTemplateQuestionDtoBase =
+      SurveyTemplateQuestionDtoBase.FromQuestionTemplateEntity(singleChoiceQuestionTemplateEntity);
+
+    // Assert
+    Assert.IsInstanceOfType(surveyTemplateQuestionDtoBase, typeof(SingleChoiceQuestionTemplateDto));
   }
 }
