151ec5bb13b9d31c69a0d116897459b2c627a2c6
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-22 20:43:15 +0300

    add test for add survey template action

--- test/SurveyApp.Test/Web/SurveyTemplate/SurveyTemplateControllerTest.cs
+++ test/SurveyApp.Test/Web/SurveyTemplate/SurveyTemplateControllerTest.cs
@@ -155,4 +155,37 @@ public sealed class SurveyTemplateControllerTest
     // Assert
     Assert.IsInstanceOfType(actionResult, typeof(CreatedAtActionResult));
   }
+
+  [TestMethod]
+  public async Task AddSurveyTemplate_AddSurveyTemplateRequestDto_ActionNamePopulated()
+  {
+    // Arrange
+    _surveyTemplateRepositoryMock.Setup(repository => repository.AddSurveyTemplateAsync(It.IsAny<SurveyTemplateEntity>(), It.IsAny<CancellationToken>()));
+
+    AddSurveyTemplateRequestDto addSurveyTemplateRequestDto = new()
+    {
+      Title = Guid.NewGuid().ToString(),
+      Description = Guid.NewGuid().ToString(),
+      Questions = new SurveyTemplateQuestionDtoBase[]
+      {
+        new TextQuestionTemplateDto
+        {
+          QuestionType = SurveyQuestionType.Text,
+          Text = Guid.NewGuid().ToString(),
+        },
+        new YesNoQuestionTemplateDto
+        {
+          QuestionType = SurveyQuestionType.YesNo,
+          Text = Guid.NewGuid().ToString(),
+        },
+      },
+    };
+
+    // Act
+    IActionResult actionResult = await _surveyTemplateController.AddSurveyTemplate(
+      addSurveyTemplateRequestDto, CancellationToken.None);
+
+    // Assert
+    Assert.AreEqual("GetSurveyTemplate", ((CreatedAtActionResult)actionResult).ActionName);
+  }
 }
