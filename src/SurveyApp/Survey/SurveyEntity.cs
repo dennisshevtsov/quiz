e1815b3bfe641f3b04c953693486c01832fbecdd
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-27 12:14:32 +0300

    create survey from template

--- src/SurveyApp/Survey/SurveyEntity.cs
+++ src/SurveyApp/Survey/SurveyEntity.cs
@@ -2,10 +2,18 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using SurveyApp.SurveyTemplate;
+
 namespace SurveyApp.Survey;
 
 public sealed class SurveyEntity
 {
+  public SurveyEntity(SurveyTemplateEntity surveyTemplateEntity)
+  {
+    Title       = surveyTemplateEntity.Title;
+    Description = surveyTemplateEntity.Description;
+  }
+
   public Guid SurveyId { get; set; }
 
   public string Title { get; set; } = string.Empty;
