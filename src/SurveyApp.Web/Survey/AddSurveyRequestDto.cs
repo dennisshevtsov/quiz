6018657447bde1a68f8bfb0aea65fcb2b077a806
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-30 23:10:09 +0300

    implement add survey request dto

--- src/SurveyApp.Web/Survey/AddSurveyRequestDto.cs
+++ src/SurveyApp.Web/Survey/AddSurveyRequestDto.cs
@@ -2,8 +2,22 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using SurveyApp.SurveyQuestion.Web;
+
 namespace SurveyApp.Survey.Web;
 
 public sealed class AddSurveyRequestDto
 {
+  public string Title { get; set; } = string.Empty;
+
+  public string Description { get; set; } = string.Empty;
+
+  public SurveyQuestionDtoBase[] Questions { get; set; } = Array.Empty<SurveyQuestionDtoBase>();
+
+  public SurveyEntity ToSurveyEntity() => new()
+  {
+    Title = Title,
+    Description = Description,
+    Questions = SurveyQuestionDtoBase.ToQuestionTemplateEntityCollection(Questions),
+  };
 }
