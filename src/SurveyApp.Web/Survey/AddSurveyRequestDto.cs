08b8df79ff47ee17b20ec688b80a2ff608887589
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-03 12:18:02 +0300

    add candidate name to dtos'

--- src/SurveyApp.Web/Survey/AddSurveyRequestDto.cs
+++ src/SurveyApp.Web/Survey/AddSurveyRequestDto.cs
@@ -13,12 +13,15 @@ public sealed class AddSurveyRequestDto : IComposable
 
   public string Description { get; set; } = string.Empty;
 
+  public string CandidateName { get; set; } = string.Empty;
+
   public SurveyQuestionDtoBase[] Questions { get; set; } = Array.Empty<SurveyQuestionDtoBase>();
 
   public SurveyEntity ToSurveyEntity() => new()
   {
-    Title = Title,
-    Description = Description,
-    Questions = SurveyQuestionDtoBase.ToQuestionTemplateEntityCollection(Questions),
+    Title         = Title,
+    Description   = Description,
+    CandidateName = CandidateName,
+    Questions     = SurveyQuestionDtoBase.ToQuestionTemplateEntityCollection(Questions),
   };
 }
