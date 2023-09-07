687fe3e6c9cc67e2b38a165af334931eece174ed
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-07 20:31:43 +0300

    add yes no enum

---
+++ src/SurveyApp/Survey/YesNo.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.Survey;
+
+public enum YesNo : byte
+{
+  None = 0,
+  Yes  = 1,
+  No   = 2,
+}
