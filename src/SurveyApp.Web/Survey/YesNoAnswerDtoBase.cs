24324404e3f0dc8db48b2810b0443635fd69734f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-08 11:59:28 +0300

    add yes not answer

---
+++ src/SurveyApp.Web/Survey/YesNoAnswerDtoBase.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.Survey.Web;
+
+public sealed class YesNoAnswerDtoBase : AnswerDtoBase
+{
+  public YesNo Answer { get; set; }
+}
