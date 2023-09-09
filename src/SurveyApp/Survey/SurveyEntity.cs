9601f52d3a6221039bb92fe5198810ca00b16597
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-09 18:18:26 +0300

    fix move to

--- src/SurveyApp/Survey/SurveyEntity.cs
+++ src/SurveyApp/Survey/SurveyEntity.cs
@@ -12,9 +12,9 @@ public sealed class SurveyEntity
 
   public SurveyEntity(SurveyTemplateEntity surveyTemplateEntity)
   {
-    Title       = surveyTemplateEntity.Title;
+    Title = surveyTemplateEntity.Title;
     Description = surveyTemplateEntity.Description;
-    Questions   = surveyTemplateEntity.Questions.Select(QuestionEntityBase.Copy).ToList();
+    Questions = surveyTemplateEntity.Questions.Select(QuestionEntityBase.Copy).ToList();
   }
 
   public Guid SurveyId { get; set; }
@@ -37,6 +37,11 @@ public sealed class SurveyEntity
     }
 
     if (state == SurveyState.Done && State != SurveyState.Ready)
+    {
+      return false;
+    }
+
+    if (State == SurveyState.Done || State == SurveyState.Cacelled)
     {
       return false;
     }
