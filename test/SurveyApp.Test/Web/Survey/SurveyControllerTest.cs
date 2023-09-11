19d401fba01232cf530711822e086ba323ab206c
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-11 20:05:30 +0300

    add test for survey controller

---
+++ test/SurveyApp.Test/Web/Survey/SurveyControllerTest.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.Survey.Web.Test;
+
+[TestClass]
+public sealed class SurveyControllerTest
+{
+#pragma warning disable CS8618
+  private Mock<ISurveyRepository> _surveyRepositoryMock;
+  private SurveyController _surveyController;
+#pragma warning restore CS8618
+
+  [TestInitialize]
+  public void Initialize()
+  {
+    _surveyRepositoryMock = new Mock<ISurveyRepository>();
+    _surveyController     = new SurveyController(_surveyRepositoryMock.Object);
+  }
+}
