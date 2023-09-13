9b03879b648737f7787db140608b2f17e273e82b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-13 16:08:45 +0300

    add test for survey entity

---
+++ test/SurveyApp.Test/Survey/SurveyEntityTest.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.Survey.Test;
+
+[TestClass]
+public sealed class SurveyEntityTest
+{
+  [TestMethod]
+  public void Constructor_FullListOfParameters_SurveyIdFilled()
+  {
+    // Arrange
+    Guid surveyId = Guid.NewGuid();
+
+    // Act
+    SurveyEntity surveyEntity = new(
+      surveyId     : surveyId,
+      state        : default,
+      title        : string.Empty,
+      description  : string.Empty,
+      candidateName: string.Empty,
+      questions    : Array.Empty<QuestionEntityBase>());
+
+    // Assert
+    Assert.AreEqual(surveyId, surveyEntity.SurveyId);
+  }
+}
