4a71c21c5d5727c3fb0b63788cdff172e7cb074a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-17 13:19:29 +0300

    add test for test question dto

---
+++ test/SurveyApp.Test/Survey/Web/TextQuestionDtoTest.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.Survey.Web.Test;
+
+[TestClass]
+public sealed class TextQuestionDtoTest
+{
+  [TestMethod]
+  public void Constructor_TextQuestionEntity_TextFilled()
+  {
+    // Arrange
+    TextQuestionEntity textQuestionEntity = new
+    (
+      text: Guid.NewGuid().ToString(),
+      answer: null
+    );
+
+    // Act
+    TextQuestionDto textQuestionDto = new(textQuestionEntity);
+
+    // Assert
+    Assert.AreEqual(textQuestionEntity.Text, textQuestionDto.Text);
+  }
+}
