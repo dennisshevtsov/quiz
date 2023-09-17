b67485af614a55f4e479f14396f8490f30b453b5
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-17 13:12:28 +0300

    add test for yes not question dto

--- test/SurveyApp.Test/Survey/Web/YesNoQuestionDtoTest.cs
+++ test/SurveyApp.Test/Survey/Web/YesNoQuestionDtoTest.cs
@@ -11,14 +11,32 @@ public sealed class YesNoQuestionDtoTest
   public void Constructor_YesNoQuestionEntity_TextFilled()
   {
     // Arrange
-    YesNoQuestionEntity yesNoQuestionEntity = new(
+    YesNoQuestionEntity yesNoQuestionEntity = new
+    (
       text  : Guid.NewGuid().ToString(),
-      answer: YesNo.None);
+      answer: YesNo.None
+    );
 
     // Act
     YesNoQuestionDto yesNoQuestionDto = new(yesNoQuestionEntity);
 
     // Assert
     Assert.AreEqual(yesNoQuestionEntity.Text, yesNoQuestionDto.Text);
+  }
+
+  [TestMethod]
+  public void ToQuestionEntity_YesNoQuestionDto_YesNoQuestionEntityReturned()
+  {
+    // Arrange
+    YesNoQuestionDto yesNoQuestionDto = new()
+    {
+      Text = Guid.NewGuid().ToString(),
+    };
+
+    // Act
+    QuestionEntityBase questionEntity = yesNoQuestionDto.ToQuestionEntity();
+
+    // Assert
+    Assert.IsInstanceOfType<YesNoQuestionEntity>(questionEntity);
   }
 }
