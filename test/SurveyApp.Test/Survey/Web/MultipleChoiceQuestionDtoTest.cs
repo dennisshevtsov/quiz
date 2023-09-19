b186fd1718a1a2bcc087956dde653130e85457bd
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-19 22:34:29 +0300

    add test for multiple choice question dto

---
+++ test/SurveyApp.Test/Survey/Web/MultipleChoiceQuestionDtoTest.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.Survey.Web.Test;
+
+[TestClass]
+public sealed class MultipleChoiceQuestionDtoTest
+{
+  [TestMethod]
+  public void Constructor_MultipleChoiceQuestionEntity_TextFilled()
+  {
+    // Arrange
+    MultipleChoiceQuestionEntity multipleChoiceQuestionEntity = new
+    (
+      text: Guid.NewGuid().ToString(),
+      choices: new[]
+      {
+        Guid.NewGuid().ToString(),
+        Guid.NewGuid().ToString(),
+        Guid.NewGuid().ToString(),
+      },
+      answers: Array.Empty<string>()
+    );
+
+    // Act
+    MultipleChoiceQuestionDto multipleChoiceQuestionDto = new(multipleChoiceQuestionEntity);
+
+    // Assert
+    Assert.AreEqual(multipleChoiceQuestionEntity.Text, multipleChoiceQuestionDto.Text);
+  }
+}
