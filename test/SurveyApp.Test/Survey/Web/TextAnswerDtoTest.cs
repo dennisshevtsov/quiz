a8241879520e6b8f89bd08313a8ffe4b466434e3
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-20 11:57:50 +0300

    add test for text answer dto

---
+++ test/SurveyApp.Test/Survey/Web/TextAnswerDtoTest.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.Survey.Web.Test;
+
+[TestClass]
+public sealed class TextAnswerDtoTest
+{
+  [TestMethod]
+  public void Update_TextQuestionEntity_AnswerUpdated()
+  {
+    // Arrange
+    TextQuestionEntity textQuestionEntity = new
+    (
+      text  : Guid.NewGuid().ToString(),
+      answer: null
+    );
+
+    TextAnswerDto textAnswerDto = new()
+    {
+      Answer = Guid.NewGuid().ToString(),
+    };
+
+    // Act
+    textAnswerDto.Update(textQuestionEntity);
+
+    // Assert
+    Assert.AreEqual(textAnswerDto.Answer, textQuestionEntity.Answer);
+  }
+}
