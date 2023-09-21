481c0fc936e4f4d2d333262e95e7e90a641bba4b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-21 12:15:06 +0300

    add test for single choice answer dto

---
+++ test/SurveyApp.Test/Survey/Web/SingleChoiceAnswerDtoTest.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.Survey.Web.Test;
+
+[TestClass]
+public sealed class SingleChoiceAnswerDtoTest
+{
+  [TestMethod]
+  public void Update_SingleChoiceQuestionEntity_AnswerUpdated()
+  {
+    // Arrange
+    string answer = Guid.NewGuid().ToString();
+
+    SingleChoiceAnswerDto singleChoiceAnswerDto = new()
+    {
+      Answer = answer,
+    };
+
+    SingleChoiceQuestionEntity singleChoiceQuestionEntity = new
+    (
+      text   : Guid.NewGuid().ToString(),
+      choices: new[]
+      {
+        Guid.NewGuid().ToString(),
+        Guid.NewGuid().ToString(),
+        answer,
+      },
+      answer : null
+    );
+
+    // Act
+    singleChoiceAnswerDto.Update(singleChoiceQuestionEntity);
+
+    // Assert
+    Assert.AreEqual(answer, singleChoiceQuestionEntity.Answer);
+  }
+}
