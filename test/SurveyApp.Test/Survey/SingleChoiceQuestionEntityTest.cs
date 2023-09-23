2de7d1a5bf6433ec8a0adfc3b735bc80b7eef0e0
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-23 19:24:09 +0300

    add test for single choice question entity

---
+++ test/SurveyApp.Test/Survey/SingleChoiceQuestionEntityTest.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.Survey.Test;
+
+[TestClass]
+public sealed class SingleChoiceQuestionEntityTest
+{
+  [TestMethod]
+  public void Constructor_Text_TextFilled()
+  {
+    // Arrange
+    string text = Guid.NewGuid().ToString();
+
+    // Act
+    SingleChoiceQuestionEntity singleChoiceQuestionEntity = new
+    (
+      text   : text,
+      choices: new[]
+      {
+        Guid.NewGuid().ToString(),
+        Guid.NewGuid().ToString(),
+        Guid.NewGuid().ToString(),
+      },
+      answer : null
+    );
+
+    // Assert
+    Assert.AreEqual(text, singleChoiceQuestionEntity.Text);
+  }
+}
