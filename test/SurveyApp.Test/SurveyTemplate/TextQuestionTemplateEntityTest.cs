471d9d0ae301ef19de28328f7fd4b42fcba04392
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-28 15:44:58 +0300

    add test for test question template entity

---
+++ test/SurveyApp.Test/SurveyTemplate/TextQuestionTemplateEntityTest.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.SurveyTemplate.Test;
+
+[TestClass]
+public sealed class TextQuestionTemplateEntityTest
+{
+  [TestMethod]
+  public void Constructor_Text_TextFilled()
+  {
+    // Arrange
+    string text = Guid.NewGuid().ToString();
+
+    // Act
+    TextQuestionTemplateEntity textQuestionTemplateEntity = new
+    (
+      text: text
+    );
+
+    // Assert
+    Assert.AreEqual(text, textQuestionTemplateEntity.Text);
+  }
+}
