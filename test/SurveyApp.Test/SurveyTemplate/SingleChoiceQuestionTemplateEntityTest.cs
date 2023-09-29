22b988b27beb05681f64fc267cffb3d23c7aee00
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-29 12:24:43 +0300

    add test for multiple choice question template

---
+++ test/SurveyApp.Test/SurveyTemplate/SingleChoiceQuestionTemplateEntityTest.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.SurveyTemplate.Test;
+
+[TestClass]
+public sealed class SingleChoiceQuestionTemplateEntityTest
+{
+  [TestMethod]
+  public void Constructor_FullListOfParameters_TextFilled()
+  {
+    // Arrange
+    string text = Guid.NewGuid().ToString();
+
+    // Act
+    SingleChoiceQuestionTemplateEntity singleChoiceQuestionTemplateEntity = new
+    (
+      text   : text,
+      choices: Array.Empty<string>()
+    );
+
+    // Assert
+    Assert.AreEqual(text, singleChoiceQuestionTemplateEntity.Text);
+  }
+}
