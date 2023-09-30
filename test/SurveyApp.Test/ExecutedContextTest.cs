c7a674b1ff0348426de7ca509018f34247122364
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-09-30 18:01:17 +0300

    add test for execution context

---
+++ test/SurveyApp.Test/ExecutedContextTest.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.Test;
+
+[TestClass]
+public sealed class ExecutedContextTest
+{
+  [TestMethod]
+  public void Ok_Null_ExceptionThrown()
+  {
+    // Act
+    Action act = () => ExecutedContext<object>.Ok(null!);
+
+    // Assert
+    Assert.ThrowsException<ArgumentNullException>(() => act());
+  }
+}
