843d12a2740311140283fe3d3185869d3835e9b3
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-10-03 20:04:09 +0300

    add executing context

---
+++ src/SurveyApp/ExecutingContext.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp;
+
+public sealed class ExecutingContext
+{
+  private readonly List<string> _errors;
+
+  public ExecutingContext()
+  {
+    _errors = new List<string>();
+  }
+
+  public IReadOnlyList<string> Errors => _errors;
+
+  public bool HasErrors => _errors.Count > 0;
+
+  public void AddError(string error) => _errors.Add(error);
+}
