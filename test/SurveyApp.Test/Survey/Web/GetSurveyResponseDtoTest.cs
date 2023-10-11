686d2c4e3dc57549c492bf9a8dc444eaa3d73611
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-10-11 22:53:15 +0300

    add test for get survey response dto

--- test/SurveyApp.Test/Survey/Web/GetSurveyResponseDtoTest.cs
+++ test/SurveyApp.Test/Survey/Web/GetSurveyResponseDtoTest.cs
@@ -2,40 +2,23 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using SurveyApp.Survey.Test;
+
 namespace SurveyApp.Survey.Web.Test;
 
 [TestClass]
 public sealed class GetSurveyResponseDtoTest
 {
   [TestMethod]
-  public void Constructor_SurveyEntity_PropertiesFilled()
+  public void Constructor_SurveyEntity_SurveyIdFilled()
   {
     // Arrange
-    SurveyEntity surveyEntity = null!;
-      //new(
-      //  surveyId     : Guid.NewGuid(),
-      //  state        : SurveyState.Draft,
-      //  title        : Guid.NewGuid().ToString(),
-      //  description  : Guid.NewGuid().ToString(),
-      //  candidateName: Guid.NewGuid().ToString(),
-      //  questions    : new QuestionEntityBase[]
-      //  {
-      //    new TextQuestionEntity(
-      //      text  : Guid.NewGuid().ToString(),
-      //      answer: Guid.NewGuid().ToString()),
-      //    new YesNoQuestionEntity(
-      //      text  : Guid.NewGuid().ToString(),
-      //      answer: YesNo.Yes)
-      //});
+    SurveyEntity surveyEntity = SurveyEntityTest.CreateTestSurvey();
 
     // Act
     GetSurveyResponseDto getSurveyResponseDto = new(surveyEntity);
 
     // Assert
     Assert.AreEqual(surveyEntity.SurveyId, getSurveyResponseDto.SurveyId);
-    Assert.AreEqual(surveyEntity.Title, getSurveyResponseDto.Title);
-    Assert.AreEqual(surveyEntity.Description, getSurveyResponseDto.Description);
-    Assert.AreEqual(surveyEntity.CandidateName, getSurveyResponseDto.CandidateName);
-    Assert.AreEqual(surveyEntity.Questions.Length, getSurveyResponseDto.Questions.Length);
   }
 }
