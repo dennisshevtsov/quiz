97e1504f36a7927dcec4080d8cc0e28afc088385
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-10-16 22:18:21 +0300

    add tests for answer of survey entity

--- test/SurveyApp.Test/Survey/SurveyEntityTest.cs
+++ test/SurveyApp.Test/Survey/SurveyEntityTest.cs
@@ -639,6 +639,30 @@ public sealed class SurveyEntityTest
   }
 
   [TestMethod]
+  public void Answer_DoneSurvey_ContextHasErrors()
+  {
+    // Assert
+    SurveyState originalState = SurveyState.Done;
+    SurveyEntity surveyEntity = SurveyEntityTest.CreateTestSurvey
+    (
+      surveyId       : default,
+      state          : originalState,
+      title          : string.Empty,
+      description    : string.Empty,
+      intervieweeName: string.Empty,
+      questions      : Array.Empty<QuestionEntityBase>()
+    );
+
+    ExecutingContext context = new();
+
+    // Act
+    surveyEntity.Answer(context);
+
+    // Assert
+    Assert.IsTrue(context.HasErrors);
+  }
+
+  [TestMethod]
   public void Answer_CancelledSurvey_StateNotUpdated()
   {
     // Assert
