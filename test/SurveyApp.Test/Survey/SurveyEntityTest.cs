5330f0852ff2a826bb66ef88f4367bf888c95081
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-10-16 22:15:16 +0300

    add tests for answer of survey entity

--- test/SurveyApp.Test/Survey/SurveyEntityTest.cs
+++ test/SurveyApp.Test/Survey/SurveyEntityTest.cs
@@ -590,6 +590,30 @@ public sealed class SurveyEntityTest
     Assert.AreEqual(originalState, surveyEntity.State);
   }
 
+  [TestMethod]
+  public void Answer_DraftSurvey_StateNotUpdated()
+  {
+    // Assert
+    SurveyState originalState = SurveyState.Draft;
+    SurveyEntity surveyEntity = SurveyEntityTest.CreateTestSurvey
+    (
+      surveyId       : default,
+      state          : originalState,
+      title          : string.Empty,
+      description    : string.Empty,
+      intervieweeName: string.Empty,
+      questions      : Array.Empty<QuestionEntityBase>()
+    );
+
+    ExecutingContext context = new();
+
+    // Act
+    surveyEntity.Answer(context);
+
+    // Assert
+    Assert.AreEqual(originalState, surveyEntity.State);
+  }
+
   private static ConstructorInfo? _surveyEntityConstructor;
 
   private static ConstructorInfo SurveyEntityConstructor => _surveyEntityConstructor ?? (_surveyEntityConstructor = GetSurveyEntityConstructor());
