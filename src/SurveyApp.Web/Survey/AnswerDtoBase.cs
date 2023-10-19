2fbae1f73ac9098e05f29db5e699930e3eaa063c
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-10-19 23:06:12 +0300

    fix answer deserialization

--- src/SurveyApp.Web/Survey/AnswerDtoBase.cs
+++ src/SurveyApp.Web/Survey/AnswerDtoBase.cs
@@ -2,8 +2,15 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using System.Text.Json.Serialization;
+
 namespace SurveyApp.Survey.Web;
 
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "questionType")]
+[JsonDerivedType(typeof(TextAnswerDto), (int)QuestionType.Text)]
+[JsonDerivedType(typeof(YesNoAnswerDto), (int)QuestionType.YesNo)]
+[JsonDerivedType(typeof(MultipleChoiceAnswerDto), (int)QuestionType.MultipleChoice)]
+[JsonDerivedType(typeof(SingleChoiceAnswerDto), (int)QuestionType.SingleChoice)]
 public abstract class AnswerDtoBase
 {
   public abstract void SetAnswer(QuestionEntityBase questionEntity, ExecutingContext context);
