f128228d28cf7a3b5b652d59546e9eeea1f64f77
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-10-21 20:33:26 +0300

    add survey entity type configuration

---
+++ src/SurveyApp.Data/Survey/SurveyEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SurveyApp.Survey.Data;
+
+public sealed class SurveyEntityTypeConfiguration : IEntityTypeConfiguration<SurveyEntity>
+{
+  public void Configure(EntityTypeBuilder<SurveyEntity> builder)
+  {
+    throw new NotImplementedException();
+  }
+}
