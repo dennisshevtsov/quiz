7930a09fa72395ef03260332f9cd2b4c1ac59d7e
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-10-21 20:27:08 +0300

    add survey template entity type configuration

---
+++ src/SurveyApp.Data/SurveyTemplate/SurveyTemplateEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SurveyApp.SurveyTemplate.Data;
+
+public sealed class SurveyTemplateEntityTypeConfiguration : IEntityTypeConfiguration<SurveyTemplateEntity>
+{
+  public void Configure(EntityTypeBuilder<SurveyTemplateEntity> builder)
+  {
+    
+  }
+}
