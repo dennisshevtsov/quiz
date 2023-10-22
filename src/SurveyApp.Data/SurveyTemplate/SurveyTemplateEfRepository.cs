b10a4873d54af77d899d3e9d33ef7e781a0ffff3
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-10-22 12:33:00 +0300

    add ef repository for survey template

---
+++ src/SurveyApp.Data/SurveyTemplate/SurveyTemplateEfRepository.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace SurveyApp.SurveyTemplate.Data;
+
+public sealed class SurveyTemplateEfRepository : ISurveyTemplateRepository
+{
+  public Task<SurveyTemplateEntity?> GetSurveyTemplateAsync(Guid surveyTemplateId, CancellationToken cancellationToken)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<SurveyTemplateEntity> AddSurveyTemplateAsync(SurveyTemplateEntity surveyTemplateEntity, CancellationToken cancellationToken)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task UpdateSurveyTemplateAsync(SurveyTemplateEntity surveyTemplateEntity, CancellationToken cancellationToken)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task DeleteSurveyTemplateAsync(Guid surveyTemplateId, CancellationToken cancellationToken)
+  {
+    throw new NotImplementedException();
+  }
+}
