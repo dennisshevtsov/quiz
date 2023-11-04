5b4825add7a32bc2879c49d03055d55a0f7341c0
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-11-04 19:56:18 +0300

    specify json property name for answer dtos

--- src/SurveyApp.Web/Survey/MultipleChoiceAnswerDto.cs
+++ src/SurveyApp.Web/Survey/MultipleChoiceAnswerDto.cs
@@ -2,10 +2,13 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using System.Text.Json.Serialization;
+
 namespace SurveyApp.Survey.Web;
 
 public sealed class MultipleChoiceAnswerDto : AnswerDtoBase
 {
+  [JsonPropertyName("answers")]
   public string[] Answers { get; set; } = Array.Empty<string>();
 
   public override void SetAnswer(QuestionEntityBase questionEntity, ExecutingContext context)
